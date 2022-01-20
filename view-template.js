52384e9bcb1bfaee0d9c3694b85a594752afed3a
eugen.zha <eugen.zha@gmail.com>
2022-01-20 23:34:57 +0300

    Try Vue

--- view-template.js
+++ view-template.js
@@ -1,29 +1,30 @@
 const viewTemplate = {
-    render: (container, data) => {
-        data.forEach(model => {
-            model.instances.forEach((props, i) => {
-                const instanceContainer = document.createElement("div");
-                container.appendChild(instanceContainer);
-
-                const titleContainer = document.createElement("div");
-                titleContainer.innerHTML = `<h3>${model.name} ${i + 1}</h3>`;
-                instanceContainer.appendChild(titleContainer);
-
-                const propsContainer = document.createElement("ul");
-                Object.keys(props).forEach(propName => {
-                    const prop = props[propName];
-                    const propItem = document.createElement("li");
-                    propItem.innerHTML = `${propName}: <b>${prop.value}</b> <i>${prop.notes || ''}</i>`;
-                    propsContainer.appendChild(propItem);
-                });
-                instanceContainer.appendChild(propsContainer);
-            });
-            
-            model.notes && model.notes.forEach(note => {
-                const notesContainer = document.createElement("p");
-                notesContainer.innerHTML = `<i>${note}</i>`;
-                container.appendChild(notesContainer);
-            });
+    render: (container, models) => {
+        const app = Vue.createApp({
+            data() {
+              return {
+                models
+              }
+            },
+            template: `
+                <div v-for="model in models">
+                    <model :model="model"></model>
+                </div>`
         });
+
+        app.component('model', {
+            props: ['model'],
+            template: `
+                <div v-for="(instance, index) in model.instances">
+                    <h3>{{model.name}} {{index + 1}}</h3>
+                    <ul>
+                        <li v-for="(prop, key) in instance">
+                            {{key}}: <b>{{prop.value}}</b> <i>{{prop.notes || ''}}</i>
+                        </li>
+                    </ul>
+                </div>`
+        });
+        
+        app.mount(container)
     }
 };
